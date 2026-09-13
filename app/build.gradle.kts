e535d649943b70948d777b1d798fc66ce77dda61
thiagueye <thiagueye@gmail.com>
2026-09-13 21:05:44 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.navilan.moi"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.navilan.moi"
         minSdk = 24
@@ -12,7 +14,17 @@ android {
         versionCode = 2
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
